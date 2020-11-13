f51bf02883066e55fe9d5e7b727fe9f6da5ef12b
dimkar1209 <dimkarmk4444@gmail.com>
2020-11-13 20:37:07 +0200

    #123213 COM test

---
+++ L8_1/L8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8
+{
+    class Program
+    {
+        [ComRegisterFunction]
+        public static void RegisterFunction(Type t)
+        {
+            var keyname = string.Format(@"SOFTWARE\Unity Technologies\{0:b}", t.GUID);
+            using (var rk = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(keyname))
+            {
+                rk.SetValue(null, 1);
+                rk.SetValue("Title", "11");
+                rk.SetValue("Description", "DD");
+            }
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterFunction(Type t)
+        {
+            var keyname = string.Format(@"SOFTWARE\Unity Technologies\{0:b}", t.GUID);
+            Microsoft.Win32.Registry.LocalMachine.DeleteSubKeyTree(keyname);
+        }
+    }
+}
